c02b7d2d9b9cd8a0a0a48392a641e2796be93e0b
saramclean17 <69061654+saramclean17@users.noreply.github.com>
2020-09-27 14:22:01 -0700

    Add files via upload
    latest PyBank commit

--- 03-Python-Challenge/PyBank/main.py
+++ 03-Python-Challenge/PyBank/main.py
@@ -9,14 +9,18 @@ csvpath=os.path.join('Resources', 'budget_data.csv')
 
 With open(csvpath,new line = ' ') as csvfile:
     csvreader=csv.reader(csvfile, delimiter ',')
+    #print csv header information
     csv_header=next(csvreader)
 
 #Declare variables
 month=[]
 revenue=[]
-average_change=[]
+revenue_change=[]
 greatest_increase_in_profits=[]
 greatest_decrease_in_profits=[]
+
+#print variable data
+print(f"Header": {csv_header})
 
 #Transfer data into variables for each row in csv.reader
 month.append(row[0])
@@ -26,5 +30,28 @@ revenue.append(row[1])
 print(len(month))
 
 #Calculate total revenue in the time period
+revenue_int=map(int, revenue)
+total_revenue=(sum(revenue_int))
+print(total_revenue)
+
+#Calculate average monthly change in revenue
+i=0
+for i in range(len(revenue)-1): profit_loss= int(revenue[i+1]) - int(revenue[i])
+revenue_change.append(profit_loss)
+total=sum(revenue_change)
+#print(revenue_change)
+monthly_change = total/len(revenue_change)
+#print(monthly_change)
+print(total)
+
+#Calculate greatest increase in profits
+profit_increase =max(revenue_change)
+print(profit_increase)
+k=revenue_change.index(profit_increase)
+month_increase = month[k+1]
+
+#Calculate greatest decrease in profits
+
+#Print output variables to Terminal
 
 
